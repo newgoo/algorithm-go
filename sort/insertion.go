da4880980db07d77a65194aead63a4b73f2af3a3
lives <lives@livess-MacBook-Pro.local>
2018-05-10 14:57:27 +0800

    shell sorting

--- sort/insertion.go
+++ sort/insertion.go
@@ -23,14 +23,17 @@ import (
 */
 func Insertion(list []int64) {
 	logrus.Info(list)
+	t := 0
 	for i := 1; i < len(list); i++ {
 		for j := 0; j < i; j++ {
 			if list[i] < list[j] {
 				list[i], list[j] = list[j], list[i]
+				t++
 			}
 		}
 	}
 	logrus.Info(list)
+	logrus.Info(t)
 	logrus.Info(algorithm.ValidSorting(list))
 }
 
--- sort/shell.go
+++ sort/shell.go
@@ -1,16 +1,29 @@
 package main
 
-import "github.com/newgoo/algorithm"
+import (
+	"github.com/newgoo/algorithm"
+	"github.com/sirupsen/logrus"
+)
 
-func Shell(list []int64, steps ...int64) {
-	var h = 1
-	var length = len(list)
-	if h < length {
-
+func Shell(list []int64, steps ...int) {
+	//h := 3
+	for _, h := range steps {
+		for i := 0; i < h; i++ {
+			for m := h + i; m < len(list); m += h {
+				for n := i; n < m; n += h {
+					if list[m] < list[n] {
+						list[m], list[n] = list[n], list[m]
+					}
+				}
+			}
+		}
 	}
+
+	logrus.Info(list)
+	logrus.Info(algorithm.ValidSorting(list))
 }
 
 func main() {
 	list := algorithm.Random(1000)
-	Shell(list, 15, 8, 2, 1)
+	Shell(list, 16, 8, 2, 1)
 }
